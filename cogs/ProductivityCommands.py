74d71ca389ad3d0d8460c5803bbc590e47830b50
dominiclye <dominiclye91@gmail.com>
2024-09-27 12:54:36 +1000

    feat: Goal configs

--- cogs/ProductivityCommands.py
+++ cogs/ProductivityCommands.py
@@ -27,7 +27,7 @@ class Productivity(commands.Cog):
         embed = discord.Embed(title=f"{ctx.author.display_name}'s Goals",  color=self.bot.settings_cache[ctx.guild.id]['color'])
         if goals:
             for goal in goals:
-                embed.add_field(name=goal["title"], 
+                embed.add_field(name=f"{goal['index']}. {goal['title']}", 
                                 value=f"Status: `{goal['status']}`",
                                 inline=False)
             
@@ -37,20 +37,96 @@ class Productivity(commands.Cog):
         await ctx.reply(embed=embed)
 
     @commands.command()
-    async def add_goal(self, ctx, *, goal):
+    async def add_goal(self, ctx, *, goal: str):
         user_id = ctx.author.id
+
         if 'goals' not in self.bot.user_settings_cache[user_id]:
             self.bot.user_settings_cache[user_id]['goals'] = []
-        
-        self.bot.user_settings_cache[user_id]['goals'].append(
-            {"title": goal, 
-             "status": "incomplete"
-            })
+
+        goals = self.bot.user_settings_cache[user_id]['goals']
+        next_index = len(goals) + 1
+
+        self.bot.user_settings_cache[user_id]['goals'].append({
+            "title": goal,
+            "status": "incomplete",
+            "index": next_index
+        })
+
         await self.settings_actions.update_user_settings(user_id, 'goals', self.bot.user_settings_cache[user_id]['goals'])
 
-        embed = discord.Embed(title="Goal Created", description=f"Added goal \"{goal}\" to goals", color=self.bot.settings_cache[ctx.guild.id]['color'])
+        embed = discord.Embed(
+            title="Goal Created", 
+            description=f"Added goal \"{goal}\" to your goals",
+            color=self.bot.settings_cache[ctx.guild.id]['color']
+        )
         embed.add_field(name="Status", value="`incomplete`")
+        embed.add_field(name="Index", value=f"`{next_index}`")
 
+        await ctx.reply(embed=embed)
+
+    @commands.command()
+    async def remove_goal(self, ctx, goal_index: int):
+        user_id = ctx.author.id
+
+        goals = self.bot.user_settings_cache.get(user_id, {}).get('goals', [])
+        embed = discord.Embed(title="Removed Goal", color=self.bot.settings_cache[ctx.guild.id]['color'])
+
+        if not goals:
+            embed.description = "There are no goals to remove."
+            return await ctx.reply(embed=embed)
+
+        if goal_index > len(goals) or goal_index < 1:
+            embed.description = "Invalid goal index."
+            return await ctx.reply(embed=embed)
+
+        removed_goal = None
+        for goal in goals:
+            if goal["index"] == goal_index:
+                removed_goal = goal
+                goals.remove(goal)
+                break
+
+        if removed_goal:
+            self.bot.user_settings_cache[user_id]['goals'] = goals
+            await self.settings_actions.update_user_settings(user_id, 'goals', goals)
+
+            embed.description = f"Successfully removed goal: {removed_goal['title']}"
+            embed.add_field(name=f"Index {removed_goal['index']}", value=f"`{removed_goal['title']}`")
+        else:
+            embed.description = "No goal found with the given index."
+
+        await ctx.reply(embed=embed)
+
+    @commands.command()
+    async def goal_status(self, ctx, goal_index: int, status):
+        user_id = ctx.author.id
+        goals = self.bot.user_settings_cache.get(user_id, {}).get('goals', [])
+        embed = discord.Embed(title="Updated Goal Status", color=self.bot.settings_cache[ctx.guild.id]['color'])
+
+        if not goals:
+            embed.description = "There are no goals to update."
+            return await ctx.reply(embed=embed)
+
+        if goal_index > len(goals) or goal_index < 1:
+            embed.description = "Invalid goal index."
+            return await ctx.reply(embed=embed)
+
+        updated_goal = None
+        for goal in goals:
+            if goal["index"] == goal_index:
+                updated_goal = goal
+                goal["status"] = status
+                break
+
+        if updated_goal:
+            self.bot.user_settings_cache[user_id]['goals'] = goals
+            await self.settings_actions.update_user_settings(user_id, 'goals', goals)
+
+            embed.description = f"Successfully updated goal: \"{updated_goal['title']}\""
+            embed.add_field(name=f"Status", value=f"`{updated_goal['status']}`")
+        else:
+            embed.description = "No goal found with the given index."
+    
         await ctx.reply(embed=embed)
 
 async def setup(bot):
